eaa068f5bbc073a2452653c3f2342302854d11dc
Nettix99 <esterpockee@gmail.com>
2026-04-23 09:33:32 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import asyncpg
 from config import DATABASE_URL
 
-pool = None
+pool: asyncpg.Pool | None = None
+
 
 async def connect():
     global pool
     pool = await asyncpg.create_pool(DATABASE_URL)
+    print("✅ DB POOL CREATED")
+
 
 async def create_tables():
     async with pool.acquire() as conn:
@@ -16,17 +19,25 @@ async def create_tables():
             cards INT DEFAULT 0,
             fragments INT DEFAULT 0,
             tree INT DEFAULT 0,
-            premium BOOLEAN DEFAULT FALSE
+            premium BOOLEAN DEFAULT FALSE,
+            cards_json TEXT DEFAULT '[]'
         );
         """)
 
-async def get_user(user_id):
-    async with pool.acquire() as conn:
-        return await conn.fetchrow("SELECT * FROM users WHERE user_id=$1", user_id)
 
-async def create_user(user_id):
+async def get_user(user_id: int):
     async with pool.acquire() as conn:
-        await conn.execute(
-            "INSERT INTO users(user_id) VALUES($1) ON CONFLICT DO NOTHING",
+        user = await conn.fetchrow(
+            "SELECT * FROM users WHERE user_id=$1",
             user_id
         )
+        return user
+
+
+async def create_user(user_id: int):
+    async with pool.acquire() as conn:
+        await conn.execute("""
+        INSERT INTO users(user_id)
+        VALUES($1)
+        ON CONFLICT DO NOTHING
+        """, user_id)
