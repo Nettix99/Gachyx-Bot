c69e95a903ebd3a5849bb9fcb761c0e459c0e112
Nettix99 <esterpockee@gmail.com>
2026-04-23 15:37:02 +0300

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,37 @@
+from database.db import cursor, conn
+
+
+def create_tables():
+
+    # 👤 Пользователи
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        candies INTEGER DEFAULT 0,
+        fragments INTEGER DEFAULT 0,
+        premium INTEGER DEFAULT 0,
+        active_card TEXT
+    )
+    """)
+
+    # 🎴 Карточки пользователя
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cards (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        name TEXT,
+        rarity TEXT,
+        count INTEGER DEFAULT 1
+    )
+    """)
+
+    # ⏱ Кулдауны
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cooldowns (
+        user_id INTEGER,
+        type TEXT,
+        time INTEGER
+    )
+    """)
+
+    conn.commit()
