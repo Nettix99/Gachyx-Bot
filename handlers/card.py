6e296f791247c059c4f47ffe08d4f3722b482403
Nettix99 <esterpockee@gmail.com>
2026-04-23 08:25:31 +0300

    Create card.py

---
+++ handlers/card.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+from aiogram.types import Message
+from db import pool
+
+router = Router()
+
+@router.message(lambda msg: msg.text.lower() in ["карта", "/card"])
+async def get_card(message: Message):
+    async with pool.acquire() as conn:
+        await conn.execute(
+            "UPDATE users SET cards = cards + 1 WHERE user_id=$1",
+            message.from_user.id
+        )
+
+    await message.answer("🎴 Ты получил карту!")
